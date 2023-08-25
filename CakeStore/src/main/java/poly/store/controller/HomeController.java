32d5d109e784c88b09964f8d7f507a61ebffef72
MSI\TienTran <tientvt1912003@gmail.com>
2023-08-25 19:14:55 +0700

    update sources

---
+++ CakeStore/src/main/java/poly/store/controller/HomeController.java
@@ -0,0 +1,31 @@
+package poly.store.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import poly.store.entity.Product;
+import poly.store.service.ProductService;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+	@Autowired
+	ProductService productService;
+	@RequestMapping({"/", "/home", "/home/index"})
+	public String index(Model model) {
+		List<Product> list = productService.findAll();
+		model.addAttribute("items", list);
+		return "product/list";
+	}
+	@RequestMapping("/about")
+	public String about() {
+		return "layout/about";
+	}
+	
+	@RequestMapping("/admin/home/index")
+	public String admin() {
+		return "redirect:/admin/index.html";
+	}
+}
